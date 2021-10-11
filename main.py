afce46fbe502f6d7b0bbf95a8511b9f2bdfefbad
Abthec <40231875+Abthec@users.noreply.github.com>
2021-10-12 12:02:40 +1300

    Ignores first entry which is '' then filters for only entires that start with '['

--- main.py
+++ main.py
@@ -3,11 +3,10 @@ from serial import Serial
 
 ser = serial.Serial("COM7", baudrate=9600, timeout=1)
 
+
 while 1:
     arduinoData = ser.readline().decode()
-    if arduinoData is not None:
+    if arduinoData != '':
         if arduinoData[0] == '[':
             print(arduinoData)
-        else:
-            continue
 
