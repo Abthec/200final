d18771e37d39ddc13cb7e51e059be77f1374696b
Abthec <40231875+Abthec@users.noreply.github.com>
2021-10-12 11:39:39 +1300

    Filters out any lines that don't start with '[' in order to target what should be the runners times

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import serial
 from serial import Serial
 
-ser = serial.Serial('COM7', baudrate=9600, timeout=1)
+ser = serial.Serial("COM7", baudrate=9600, timeout=1)
 
 while 1:
-    arduinoData = ser.readline()
-    print(arduinoData)
+    arduinoData = ser.readline().decode()
+    if arduinoData[0] == '[':
+        print(arduinoData)
+    else:
+        continue
+
